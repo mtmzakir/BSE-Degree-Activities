c874a091e14f83ee06632c108f10c5d0a59073d6
Zakir MTM <90142607+mtmzakir@users.noreply.github.com>
2022-08-25 19:06:50 +0530

    Add files via upload

---
+++ Python/Fish Tank Water Capacity Calculator/Fish Tank Water Capacity Calculator.py
@@ -0,0 +1,6 @@
+print("Fish Tank Water Capacity Calculator made by M.T.M. Zakir")
+width=float(input("Please enter Length of the Tank:")) 
+length=float(input("Please enter Width of Tank:")) 
+height=float(input("Please enter Height of Tank:")) 
+volume=width*length*height 
+print("The Volume of the Tank is:",volume)
